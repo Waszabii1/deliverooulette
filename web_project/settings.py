ad65fd12d3852fc26e5bba1b9f68daf06c5be7f8
Waszabii1 <will.grenyer@hotmail.co.uk>
2024-01-21 17:00:49 +0000

    Update settings.py

--- web_project/settings.py
+++ web_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uckpq8-&!%=!fj_ks41j66wjpkz9e*na7xqej4ggbri4uz-#+!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
