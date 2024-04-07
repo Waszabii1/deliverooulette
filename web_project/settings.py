409d4ee060f74e1a2870a060d5f6fda839ebe6b8
Waszabii1 <will.grenyer@hotmail.co.uk>
2024-04-07 17:36:28 +0100

    wad
    awd

--- web_project/settings.py
+++ web_project/settings.py
@@ -74,7 +74,8 @@ TEMPLATES = [
 
 CSRF_TRUSTED_ORIGINS = ["https://*.railway.app",
                         "https://deliverooulette.com*",
-                        "https://deliverooulette.com"
+                        "https://deliverooulette.com",
+                        "https://www.deliverooulette.com"
 ]
 
 WSGI_APPLICATION = 'web_project.wsgi.application'
